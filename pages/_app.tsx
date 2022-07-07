790acedde47a606466ad9a9e8b4c9628e8e2bfea
Shadab Saifi <shadabsaifi03@gmail.com>
2022-07-07 23:52:19 +0530

    google verification tag added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <meta name="google-site-verification" content="iFh3v6qlT8yo54OeWECGp7zxaGuYhf0FT01Rgpsif78" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
